96964ab5f88d90e4e9e26442dd9addc831856158
Lukas Seglias <lukas.seglias@gmail.com>
2020-07-15 19:18:10 +0200

    Finally autotiling works

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -7,6 +7,7 @@ const MAX_SPEED = 100
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
 onready var sprite = $Sprite
+onready var weapon = $Weapon
 
 func _ready():
 	pass
@@ -18,18 +19,8 @@ func _process(delta):
 
 func _physics_process(delta):
 	var input = _getMovementInputVector()
-	if input != Vector2.ZERO:
-		if input.x > 0:
-			sprite.flip_h = false
-		elif input.x < 0:
-			sprite.flip_h = true
-		animationPlayer.play("RunLeft")
-		velocity = velocity.move_toward(input * MAX_SPEED, ACCELERATION * delta)
-	else:
-		animationPlayer.play("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	velocity = move_and_slide(velocity)
+	_playMovementAnimation(input)
+	_moveBasedOnInput(delta, input)
 
 
 func _getMovementInputVector():
@@ -37,3 +28,30 @@ func _getMovementInputVector():
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), 
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	).normalized()
+
+
+func _moveBasedOnInput(delta, input):
+	if input != Vector2.ZERO:
+		velocity = velocity.move_toward(input * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	velocity = move_and_slide(velocity)
+
+
+func _playMovementAnimation(input):
+	if input != Vector2.ZERO:
+		if input.x > 0:
+			sprite.flip_h = false
+			weapon.rotation_degrees = 90
+		elif input.x < 0:
+			sprite.flip_h = true
+			weapon.rotation_degrees = -90
+		else:
+			pass # do not flip
+		animationPlayer.play("RunLeft")
+	else:
+		animationPlayer.play("Idle")
+
+
+func _on_Coin_body_entered(body):
+	print("Coin collected!")
