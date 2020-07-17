441f6f1335f765728ccc687117deceb563f12825
LucaRitz <ritz.luca@gmail.com>
2020-07-17 08:57:36 +0200

    Fixed coin collision

--- Scenes/Coin.gd
+++ Scenes/Coin.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
 onready var coinAudio = $CoinAudio
-onready var hitBox = $CollisionShape2D
+onready var hitBox = $Area2D
 
 func _ready():
-	connect("body_entered", self, "_on_body_entered")
+	pass
 
 
 func _on_body_entered(body):
+	hitBox.disconnect("body_entered", self, "_on_body_entered")
 	if(body.is_in_group("Player")):
-		hitBox.disabled = true
 		visible = false
 		coinAudio.play()
 		body.collectCoin(self)
