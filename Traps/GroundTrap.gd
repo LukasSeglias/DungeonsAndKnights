1dcf9f939be7af21fedea1e0d15fac99dd37d050
LucaRitz <ritz.luca@gmail.com>
2020-07-21 10:46:52 +0200

    Added one way trap

--- Traps/GroundTrap.gd
+++ Traps/GroundTrap.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+export(bool) var oneWay = false
+
 onready var animation = $AnimationPlayer
+var animationPlayed = false
 
 func _on_ActivationArea_body_entered(body):
-	animation.get_animation("TrapAnimation").set_loop(true)
-	animation.play("TrapAnimation")
+	if not oneWay:
+		animation.get_animation("TrapAnimation").set_loop(true)
+		animation.play("TrapAnimation")
+	elif not animationPlayed:
+		animation.play("TrapAnimation_OneWay")
+	animationPlayed = true
 
 func _on_ActivationArea_body_exited(body):
 	animation.get_animation("TrapAnimation").set_loop(false)
